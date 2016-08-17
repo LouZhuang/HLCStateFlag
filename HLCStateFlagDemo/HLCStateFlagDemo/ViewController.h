39fd96da0be758c7cb47163190b2bd32a4420b8d
huanglch <416715288@qq.com>
2016-08-17 11:23:49 +0800

    first

---
+++ HLCStateFlagDemo/HLCStateFlagDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLCStateFlagDemo
+//
+//  Created by huanglch on 16/8/17.
+//  Copyright © 2016年 huanglch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLCStateFlagDemo/HLCStateFlagDemo/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  HLCStateFlagDemo
+//
+//  Created by huanglch on 16/8/17.
+//  Copyright © 2016年 huanglch. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"哈哈");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
